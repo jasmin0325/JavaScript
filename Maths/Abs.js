8614bd48b9c0ce8a8e41760f72a820b00a053c6a
ijjamminn <lee.ja.min325@gmail.com>
2022-12-31 10:10:47 +0900

    area

--- Maths/Abs.js
+++ Maths/Abs.js
@@ -9,14 +9,18 @@
  * @example abs(0) = 0
  */
 
-const abs = (num) => {
-  const validNumber = +num // converted to number, also can use - Number(num)
+function abs(num) {
+    const validNumber = +num // converted to number, also can use - Number(num)
 
-  if (Number.isNaN(validNumber)) {
-    throw new TypeError('Argument is NaN - Not a Number')
-  }
+    if (Number.isNaN(validNumber)) {
+        throw new TypeError('Argument is NaN - Not a Number')
+    }
 
-  return validNumber < 0 ? -validNumber : validNumber // if number is less then zero mean negative then it converted to positive. i.e -> n = -2 = -(-2) = 2
+    return validNumber < 0 ? -validNumber : validNumber // if number is less then zero mean negative then it converted to positive. i.e -> n = -2 = -(-2) = 2
 }
 
 export { abs }
+console.log(abs(-10))
+console.log(abs(5))
+
+console.log(abs(-2))
